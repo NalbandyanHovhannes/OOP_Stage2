e1f49fa7045bf01d20de6790f404ceac92e45ca0
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2023-01-09 00:19:46 +0400

    Generic method

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TestGenClass<int> testGenClass = new TestGenClass<int>();
+            //testGenClass.TestGenMeth<int>();
+
+            TestGenClass<string> name = new TestGenClass<string>();
+            name.value = "TestNalbandyan";
+
+            // instance of float type
+            TestGenClass<float> version = new TestGenClass<float>();
+            version.value = 5.0F;
+
+            // display Nalbandyan
+            Console.WriteLine(name.value);
+
+            // display 5
+            Console.WriteLine(version.value);
+
+
+            TestGenClass<int> testGenClass2 = new TestGenClass<int>();
+
+            // calling Generics method
+            testGenClass2.Display<int>("Integer", 122);
+            testGenClass2.Display<char>("Character", 'H');
+            testGenClass2.Display<double>("Decimal", 255.67);           
+
         }
     }
 }
---
+++ Generics/TestGenClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class TestGenClass<T>
+    {
+        // private data members
+        private T data;
+
+        // using properties
+        public T value
+        {
+            // using accessors
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+        // Generics method
+        public void Display<TypeOfValue>(string msg, TypeOfValue value)
+        {
+            Console.WriteLine("{0}:{1}", msg, value);
+        }
+    }
+}
